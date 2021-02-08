ff4d1fbf6154baafddb119c7e63959bdd1a7d8ef
hdemusg <sumedhgarimella@gmail.com>
2021-02-08 09:53:36 -0500

    Got Google Auth, Calendar add, and Spotify profile link working.

--- app/src/main/java/io/github/hdemusg/flymontag/MainActivity.java
+++ app/src/main/java/io/github/hdemusg/flymontag/MainActivity.java
@@ -1,12 +1,20 @@
 package io.github.hdemusg.flymontag;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseUser;
+
+import android.net.Uri;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
@@ -14,22 +22,24 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import io.github.hdemusg.flymontag.ui.project.ProfileFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    static SignInButton signin = null;
+    GoogleSignInOptions gso = null;
+    GoogleSignInAccount account = null;
+
+    public static GoogleSignInClient mGoogleSignInClient = null;
+
+    public static GoogleSignInClient getClient() {
+        return mGoogleSignInClient;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
-        GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-
-        if (account != null) {
-
-        }
+        //FirebaseApp.initializeApp(this);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -42,6 +52,34 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        account = GoogleSignIn.getLastSignedInAccount(this);
+
+        if (account != null) {
+            ProfileFragment.setAccount(account.getEmail());
+        }
+
+        signin = ProfileFragment.getSignin();
+        /*
+        signin.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Uri uriUrl = Uri.parse("spotify:artist:7KlvKwcScxowO6q1tSKApu");
+                        Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+                        startActivity(launchBrowser);
+                    }
+                }
+        );
+
+         */
     }
 
 }
--- app/src/main/java/io/github/hdemusg/flymontag/ui/project/ProfileFragment.java
+++ app/src/main/java/io/github/hdemusg/flymontag/ui/project/ProfileFragment.java
@@ -3,6 +3,7 @@ package io.github.hdemusg.flymontag.ui.project;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,15 +17,39 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import io.github.hdemusg.flymontag.MainActivity;
 import io.github.hdemusg.flymontag.R;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class ProfileFragment extends Fragment {
 
     private ProfileViewModel profileViewModel;
-    EditText email = null;
+    static SignInButton signin = null;
+    static TextView email = null;
+    static Button signout = null;
+    private FirebaseAuth mAuth = null;
+
+    View root = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         profileViewModel =
                 ViewModelProviders.of(this).get(ProfileViewModel.class);
         View root = inflater.inflate(R.layout.fragment_profile, container, false);
@@ -35,13 +60,33 @@ public class ProfileFragment extends Fragment {
         //       textView.setText(s);
         //    }
         //});
-        Button signin = (Button) root.findViewById(R.id.signin);
+// ...
+// Initialize Firebase Auth
+        FirebaseApp.initializeApp(getContext());
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            setAccount(currentUser.getEmail());
+        }
+
+        signin = (SignInButton) root.findViewById(R.id.signin);
+
+        signout = (Button) root.findViewById(R.id.signout);
+        signout.setVisibility(View.GONE);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                setAccount(null);
+            }
+        });
+
+        final GoogleSignInClient client = MainActivity.getClient();
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri uriUrl = Uri.parse("spotify:artist:7KlvKwcScxowO6q1tSKApu");
-                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-                startActivity(launchBrowser);
+                Intent signInIntent = client.getSignInIntent();
+                startActivityForResult(signInIntent, 1);
             }
         });
 
@@ -50,9 +95,69 @@ public class ProfileFragment extends Fragment {
         return root;
     }
 
-    public void setAccount(String e) {
-        if (email != null) {
-            email.setText(e);
+    public static SignInButton getSignin() {
+        return signin;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == 1) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleSignInResult(task);
         }
+    }
+
+    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+        try {
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+            firebaseAuthWithGoogle(account.getIdToken());
+            // Signed in successfully, show authenticated UI.
+            setAccount(account.getEmail());
+        } catch (ApiException e) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w(TAG, "Google sign in failed", e);
+        }
+    }
+
+    public static void setAccount(String e) {
+        if (email != null) {
+            if (e != null) {
+                email.setText(e);
+                signout.setVisibility(View.VISIBLE);
+                signin.setVisibility(View.GONE);
+            } else {
+                email.setText("You are not logged in.");
+                signout.setVisibility(View.GONE);
+                signin.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(String idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            setAccount(user.getEmail());
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                        }
+
+                        // ...
+                    }
+                });
     }
 }
